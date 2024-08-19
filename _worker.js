12ebfad4e7863cec9bfe4feeb90cdba08b483bbe
几番春秋 <66902293+cheng-yyds@users.noreply.github.com>
2024-08-19 19:02:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,9 +2,9 @@
 // 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
 
 let mytoken = 'goodluck'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
-let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
-let ChatID =''; //可以为空，或者@userinfobot中获取，/start
-let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
+let BotToken ='7465555346:AAHIvfIjj1lWCw9E9T-1mqJcyMtC7x7gRPE'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
+let ChatID ='994890022'; //可以为空，或者@userinfobot中获取，/start
+let TG = 1; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'goodluck-Workers';
 let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
 let total = 520;//TB
