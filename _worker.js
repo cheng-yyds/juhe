1d306776fe70ef0a04ec2c2deb665cddc7cb69af
几番春秋 <66902293+cheng-yyds@users.noreply.github.com>
2024-08-20 16:03:15 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 
 let mytoken = 'goodluck'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
 let BotToken ='7465555346:AAHIvfIjj1lWCw9E9T-1mqJcyMtC7x7gRPE'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
-let ChatID ='994890022，1997631597'; //可以为空，或者@userinfobot中获取，/start
+let ChatID ='994890022,1997631597'; //可以为空，或者@userinfobot中获取，/start
 let TG = 1; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'goodluck-Workers';
 let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
